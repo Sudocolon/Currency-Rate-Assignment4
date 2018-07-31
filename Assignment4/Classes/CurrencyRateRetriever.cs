612e1037085ce386bc0be7afcc2e5de3d6432b38
Geron Adams <geronadams@gmail.com>
2018-07-31 11:12:05 -0500

    xml ret

---
+++ Assignment4/Classes/CurrencyRateRetriever.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Assignment4.Classes
+{
+    public static class CurrencyRateRetriever
+    {
+        static string Url;
+        static XmlDocument Xml;
+        static CurrencyRateRetriever()
+        {
+            Xml = new XmlDocument();
+        }
+        public static void SetUrl(string url)
+        {
+            Url = url;
+        }
+        public static string GetUrl()
+        {
+            return Url;
+        }
+        public static XmlDocument GetXml()
+        {
+            return Xml;
+        }
+        public static void RetrieveXml(string url)
+        {
+            Xml.Load(url);
+        }
+    }
+}
--- Assignment4/Tests/CurrencyRateRetrieverTest.cs
+++ Assignment4/Tests/CurrencyRateRetrieverTest.cs
@@ -1,9 +1,11 @@
-﻿using NUnit.Framework;
+﻿using Assignment4.Classes;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Assignment4.Tests
 {
@@ -13,6 +15,21 @@ namespace Assignment4.Tests
         public void PassTest()
         {
             Assert.AreEqual(true, true);
+        }
+        [Test]
+        public void InputURLTest()
+        {
+            CurrencyRateRetriever.SetUrl("urlvalue");
+            Assert.AreEqual("urlvalue", CurrencyRateRetriever.GetUrl());
+        }
+        [Test]
+        public void GetXMLTest()
+        {
+            XmlDocument emptyXML = new XmlDocument();
+            string url = "http://rates.fxcm.com/RatesXML";
+            Assert.AreEqual(emptyXML, CurrencyRateRetriever.GetXml());
+            CurrencyRateRetriever.RetrieveXml(url);
+            Assert.AreNotEqual(emptyXML, CurrencyRateRetriever.GetXml());
         }
     }
 }
